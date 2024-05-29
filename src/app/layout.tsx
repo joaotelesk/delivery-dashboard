691c0a4b7cd9b64d7280ea42a5d1ef0f18b7e621
Joao Teles <joaojoseteles@gmail.com>
2024-05-29 10:14:49 -0300

    :sparkles: feat: adding sound toaster and improving text content quality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
 import '@/styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={(inter.className, 'antialiased')}>
+        <Toaster richColors />
+        {children}
+      </body>
     </html>
   )
 }
